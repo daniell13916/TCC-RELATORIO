98e1fabf5194cd0ffdfc26383156752829d28cf4
daniell13916 <152323615+daniell13916@users.noreply.github.com>
2024-06-23 19:53:03 -0300

    Update TCC_RELATORIO.py

--- TCC_RELATORIO.py
+++ TCC_RELATORIO.py
@@ -559,7 +559,7 @@ def generate_report(senha_empresa, data_inicio, data_fim):
     except psycopg2.Error as e:
         st.error(f"Erro ao conectar no banco de dados: {e}")
 
-
+# Interface do usuário utilizando Streamlit
 def collection_form():
     st.markdown("<h1 style='color: #38b6ff;'>Relatório de Coleta</h1>", unsafe_allow_html=True)
 
@@ -573,28 +573,25 @@ def collection_form():
         senha_empresa = st.text_input("Senha da Empresa", type="password")
         
         residuos = {}
-        tipo_residuo = st.selectbox("Tipo de Resíduo", [
-            "Nenhum elemento", "plastico", "vidro", "papel", "papelao", "aluminio", "aco",
-            "residuos_eletronicos", "pilhas_baterias", "folhas_galhos",
-            "tetrapak", "pneus", "oleo_cozinha", "cds_dvds", "cartuchos_tinta",
-            "entulho_construcao", "madeira", "paletes", "serragem",
-            "produtos_quimicos", "medicamentos", "lampadas_fluorescentes",
-            "materia_organica", "cobre"
-        ])
-        
-        adicionar_elemento = st.form_submit_button("Adicionar elemento")
-        while tipo_residuo != "Nenhum elemento":
-            if adicionar_elemento:
-                volume = st.number_input(f"Volume Coletado de {tipo_residuo} (Kg)", min_value=0.01, key=f"volume_{len(residuos)}")
-                residuos[tipo_residuo] = volume
-                tipo_residuo = st.selectbox("Tipo de Resíduo", [
-                    "Nenhum elemento", "plastico", "vidro", "papel", "papelao", "aluminio", "aco",
-                    "residuos_eletronicos", "pilhas_baterias", "folhas_galhos",
-                    "tetrapak", "pneus", "oleo_cozinha", "cds_dvds", "cartuchos_tinta",
-                    "entulho_construcao", "madeira", "paletes", "serragem",
-                    "produtos_quimicos", "medicamentos", "lampadas_fluorescentes",
-                    "materia_organica", "cobre"
-                ], key=f"tipo_residuo_{len(residuos)}")
+        while True:
+            tipo_residuo = st.selectbox("Tipo de Resíduo", [
+                "Nenhum elemento", "plastico", "vidro", "papel", "papelao", "aluminio", "aco",
+                "residuos_eletronicos", "pilhas_baterias", "folhas_galhos",
+                "tetrapak", "pneus", "oleo_cozinha", "cds_dvds", "cartuchos_tinta",
+                "entulho_construcao", "madeira", "paletes", "serragem",
+                "produtos_quimicos", "medicamentos", "lampadas_fluorescentes",
+                "materia_organica", "cobre"
+            ], key=f"tipo_residuo_{len(residuos)}")
+
+            if tipo_residuo == "Nenhum elemento":
+                break
+            
+            volume = st.number_input(f"Volume Coletado de {tipo_residuo} (Kg)", min_value=0.01, key=f"volume_{len(residuos)}")
+            residuos[tipo_residuo] = volume
+            
+            adicionar_elemento = st.form_submit_button("Adicionar elemento")
+            if not adicionar_elemento:
+                break
 
         submit_button_cadastro = st.form_submit_button("Registrar Coleta")
         if submit_button_cadastro:
@@ -602,6 +599,5 @@ def collection_form():
             st.write(result_message)
 
 collection_form()
-
 # Executar o site
 home()
