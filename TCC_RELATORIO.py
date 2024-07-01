1e7f137e1c9b9c453b006bcee77e820b3977bc7b
daniell13916 <152323615+daniell13916@users.noreply.github.com>
2024-07-01 11:44:16 -0300

    Update TCC_RELATORIO.py

--- TCC_RELATORIO.py
+++ TCC_RELATORIO.py
@@ -457,9 +457,6 @@ def generate_report(senha_empresa, data_inicio, data_fim):
             st.write(f"No período entre {data_inicio_formatada} a {data_fim_formatada} foram feitas {total_coletas} coletas, totalizando cerca de {round(volume_total, 2)} kg coletados.")
             st.write(f"Foi considerada uma perda de {round(nao_reciclado, 2)} kg de rejeito ou materiais não recicláveis nos recipientes de coleta.")
             st.write(f"Ao final do período conseguimos destinar corretamente {round(volume_destinado_corretamente, 2)} kg, reinserindo-os na economia circular, através da reciclagem e da compostagem.")
-
-            st.markdown("<h2 style='color: #38b6ff;'>Análise Gravimétrica</h2>", unsafe_allow_html=True)
-            st.write("Porcentagem de cada tipo de material em relação ao peso total")
     
             # Calcular economias com base nas proporções
             proporcoes = buscar_valores_proporcoes(senha_empresa, data_inicio, data_fim)
@@ -523,6 +520,8 @@ def generate_report(senha_empresa, data_inicio, data_fim):
         st.error("Dados sobre as proporções de resíduos ausentes. Peça para o moderador fazer uma avaliação ou inserir os dados após a análise.")
     except psycopg2.Error as e:
         st.error(f"Erro ao conectar no banco de dados: {e}")
+
+
 
 
 # Função para exibir o formulário de coleta
