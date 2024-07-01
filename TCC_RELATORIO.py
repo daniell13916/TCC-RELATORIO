c525c7c101b5a2f7b877d153dc8dd2d65ac6ab35
daniell13916 <152323615+daniell13916@users.noreply.github.com>
2024-07-01 09:21:49 -0300

    Update TCC_RELATORIO.py

--- TCC_RELATORIO.py
+++ TCC_RELATORIO.py
@@ -460,8 +460,8 @@ def generate_report(senha_empresa, data_inicio, data_fim):
     
                 if porcentagem_rejeitos is not None:
                     porcentagem_rejeitos = float(porcentagem_rejeitos[0])  # Converter para float
-                         # Buscar valores para criar gráfico e obter dados necessários
-                        dados_empresa = buscar_valores_e_criar_grafico(senha_empresa, data_inicio, data_fim)
+                    #Buscar valores para criar gráfico e obter dados necessários
+                    dados_empresa = buscar_valores_e_criar_grafico(senha_empresa, data_inicio, data_fim)
     
                     if dados_empresa:
                         volume_total = dados_empresa[0]
